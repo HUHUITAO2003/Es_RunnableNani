5d50534236b8bfea270546cb54d46984e0d9c8aa
HU_HUI_TAO <huitao.huangelo@gmail.com>
2021-02-15 21:08:35 +0100

    Es_RunnableNani

---
+++ src/settenani/Run.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package settenani;
+
+/**
+ *
+ * @author juliet
+ */
+public class Run implements Runnable {
+    private String name;
+    
+    public Run(String name) {
+        this.name=name;
+    }
+    
+    @Override
+    public void run(){
+        for(int i=1;i<8;i++){
+        System.out.println(i + " Mi chiamo " + name);
+        }
+    }
+
+}
